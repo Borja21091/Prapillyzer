7575c7ca8cf05e8bc34a5eaef136a3a745c91442
Borja Marin <bmarin003@gmail.com>
2024-06-28 14:09:48 +0100

    test working

--- superfeatures.py
+++ superfeatures.py
@@ -33,20 +33,22 @@ def cdr_profile(mask:np.ndarray, ang_step:int=15) -> list:
     ellipses = []
     for i in np.unique(mask)[1:]:
         ellipse = cv2.fitEllipse(np.argwhere(mask == i))
+        ellipse = ((ellipse[0][1], ellipse[0][0]), (ellipse[1][1], ellipse[1][0]), ellipse[2])
         ellipses.append(ellipse)
         
     # Sort ellipses by area, smallest (cup) to largest (disc)
     ellipses = sorted(ellipses, key=lambda x: x[1][0]*x[1][1])
+    x0, y0 = ellipses[0][0]
     
-    # Intersection line (y = mx + n) with ellipses ((x-x0)^2/A^2 + (y-y0)^2/B^2 = 1)
-    m = np.array([np.tan(np.deg2rad(ang)) for ang in range(0, 180, ang_step)])
+    # Intersection line with ellipses
+    m = np.array([np.tan(np.deg2rad(ang)) for ang in np.arange(0, 180, ang_step)])
     n = np.array([ellipses[0][0][1] - m_ * ellipses[0][0][0] for m_ in m])
-    intersections = [intersection_line_ellipse(m, n, e) for e in ellipses]
+    intersections = [intersection_line_ellipse(m, n, e, x0, y0) for e in ellipses]
     
     # Compute cup-to-disc ratio profile
     centre = np.array(ellipses[0][0]).reshape(-1,1)
-    radii = np.array([distance.cdist(centre.T,i.T) for i in intersections])
-    cdr_profile = radii[0,:] / radii[1,:]
+    radii = np.array([distance.cdist(centre.T, i.T) for i in intersections])
+    cdr_profile = radii[0,:] / radii[1,:]   
     
     # Prepare output
     out = []
@@ -56,16 +58,51 @@ def cdr_profile(mask:np.ndarray, ang_step:int=15) -> list:
     
     return out
     
-def intersection_line_ellipse(m, n, ellipse):
-    x0, y0 = ellipse[0]
-    A, B = ellipse[1]
-    C = 1 - (x0/A)**2 - (y0/B)**2
-    a = B**2 + m**2*A**2
-    b = 2 * A**2*m*(n - y0) - 2*x0*B**2
-    c = A**2 * (n**2 - 2*n*y0 - C*B**2)
+def intersection_line_ellipse(m, n, ellipse, x0, y0):
+    
+    A, B, C, D, E, F = implicit_ellipse(ellipse)
+    
+    # Intersection points
+    a = A + m*(B + m*C)
+    b = D + m*(2*C*n + E) + B*n
+    c = n*(C*n + E) + F
     delta = b**2 - 4*a*c
     x = np.array([(-b + np.sqrt(delta)) / (2*a), (-b - np.sqrt(delta)) / (2*a)])
     x = x.reshape(-1, 1) # Reshape to column vector
+    # x = np.vstack((x[0,:].T, x[1,::-1].T)).reshape(-1, 1)
     y = np.tile(m,2).reshape(-1,1)*x + np.tile(n,2).reshape(-1,1)
     
-    return np.array([x.flatten(), y.flatten()])
+    # Replace zero values with nan (vertical intersections)
+    y[np.isclose(y, 0)] = np.nan
+    
+    # Handle vertical intersections
+    y_ = np.roots([C, B*x0 + E, A*x0**2 + D*x0 + F])
+    y_ = np.sort(y_, axis=0)[::-1] # Sort in descending order
+    
+    # Replace nan values with vertical intersections
+    x[np.isnan(y)] = x0
+    y[np.isnan(y)] = y_
+    
+    # Prepare output
+    out = np.array([x.flatten(), y.flatten()])
+    # Sort circularly from 0 to 360 degrees
+    ang = np.arctan2(out[1,:] - y0, out[0,:] - x0) % (2*np.pi)
+    idx = np.argsort(ang)
+    out = out[:,idx]
+    
+    return out
+    
+def implicit_ellipse(ellipse):
+        
+    x0, y0 = ellipse[0]
+    a, b = tuple(ti/2 for ti in ellipse[1])
+    theta = np.deg2rad(ellipse[2])
+    
+    A = a**2*np.sin(theta)**2 + b**2*np.cos(theta)**2
+    B = 2*(b**2 - a**2)*np.sin(theta)*np.cos(theta)
+    C = a**2*np.cos(theta)**2 + b**2*np.sin(theta)**2
+    D = -2*A*x0 - B*y0
+    E = -B*x0 - 2*C*y0
+    F = A*x0**2 + B*x0*y0 + C*y0**2 - a**2*b**2
+    
+    return A, B, C, D, E, F
--- tests/cdr_profile.py
+++ tests/cdr_profile.py
@@ -18,14 +18,16 @@ if __name__ == '__main__':
     mask = np.array(mask)
     
     # Compute cup-to-disc ratio profile
-    sec_cup, sec_disc, cdr = cdr_profile(mask)
+    sec_cup, sec_disc, cdr = cdr_profile(mask, ang_step=5)
     
     # Plot of profile
     plt.figure()
-    plt.plot(cdr[0,:], cdr[1,:])
+    plt.plot(cdr[0,:], cdr[1,:], 'k--', linewidth=0.5)
+    plt.scatter(cdr[0,:], cdr[1,:], s=3, c='k')
     plt.xlabel('Angle (degrees)')
     plt.ylabel('Cup-to-disc ratio')
     plt.title('Cup-to-disc ratio profile')
+    plt.ylim([0, 1])
     plt.grid()
     
     # Plot figure with intersection points
@@ -33,7 +35,18 @@ if __name__ == '__main__':
     for sec, color in zip([sec_cup, sec_disc], ['r', 'b']):
         x = sec[0]
         y = sec[1]
-        plt.plot(y, x, color + 'o')
+        plt.plot(x, y, color + 'o', markersize=2)
+    # Fit ellipse to cup
+    cup = cv2.fitEllipse(np.argwhere(mask == 1))
+    cup = ((cup[0][1], cup[0][0]), (cup[1][1], cup[1][0]), cup[2])
+    cv2.ellipse(img, cup, (0, 255, 0), 2)
+    # Fit ellipse to disc
+    disc = cv2.fitEllipse(np.argwhere(mask == 2))
+    disc = ((disc[0][1], disc[0][0]), (disc[1][1], disc[1][0]), disc[2])
+    cv2.ellipse(img, disc, (0, 0, 255), 2)
+    # Plot the lines passing through the centre of the cup
+    for i in range(len(sec_cup[0])):
+        plt.plot([sec_cup[0][i], sec_disc[0][i]], [sec_cup[1][i], sec_disc[1][i]], 'k--', linewidth=0.5)
     plt.imshow(img)
     
     plt.show()
